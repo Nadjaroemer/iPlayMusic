2111be70b9dfbbf1cf38923bffe4fa434f0bf491
Nadjaroemer <nadja.kuti@gmail.com>
2021-01-20 08:39:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <div className="app">
+      <Welcome/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import "./Welcome.css";
+
+function Welcome(){
+    return(
+        <div className="welcome">
+            <img className="welcome__image" src="./images/music-logo-solid.png" alt="iplaymusiclogo"/>
+            <h1>iPlayMusic</h1>
+        </div>
+    )
+}
+
+export default Welcome;
