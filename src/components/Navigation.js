5c9a89ed4029dff3050c276a6f34ff42892abc0b
Nadjaroemer <nadja.kuti@gmail.com>
2021-01-20 13:40:18 +0100

    add navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import "./Navigation.css";
+import "./Variables.css";
+
+export default function Navigation() {
+    return(
+        <section className="navigation">
+            <img src="./images/pulse_icon.png" alt=""/>
+            <img src="./images/microphone2_icon.svg" alt=""/>
+      
+            <div className="navigation__wifiIcon"> 
+                <img src="./images/Pfad 3.png" alt=""/>
+                <img src="./images/wifi_icon.png" alt=""/>
+            </div>
+            
+            <img src="./images/contrast_icon.png" alt=""/>
+            <img src="./images/featured_icon.png" alt=""/>
+        </section>
+    )
+}
