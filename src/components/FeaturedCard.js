f192146f4e21f108a336afe97809c2ad1b364c62
Nadjaroemer <nadja.kuti@gmail.com>
2021-02-01 10:29:57 +0100

    changed headline

--- src/components/FeaturedCard.js
+++ src/components/FeaturedCard.js
@@ -1,12 +1,12 @@
 //FeaturedCard.js
 import "./Featured.css"
 
-function FeaturedCard({name,description,image}) {
+function FeaturedCard({description,image}) {
     return(
         <article className="featured">
             <img className="featured__image" src={image} alt="albumcover"/>
             <div className="featured__headlines">
-                <h1 className="featured__h1">{name}</h1>
+                
                 <h2 className="featured__h2">{description}</h2>
             </div>
         </article>
